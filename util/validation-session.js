20c765ed47b9f6585c750289426be5365b00869a
Božomil Milanović <bozomil@gmail.com>
2024-12-28 06:45:25 +0100

    Refactoring: Sessions and input validations errors

---
+++ util/validation-session.js
@@ -0,0 +1,28 @@
+function getSessionErrorData(req, defaultValues) {
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      ...defaultValues
+    };
+  }
+
+  req.session.inputData = null;
+
+  return sessionInputData;
+}
+
+function flashErrorsToSession(req, data, action) {
+  req.session.inputData = {
+    hasError: true,
+    message: 'Invalid input - please check your data.',
+    ...data
+  };
+  req.session.save(action);
+}
+
+module.exports = {
+  getSessionErrorData: getSessionErrorData,
+  flashErrorsToSession: flashErrorsToSession,
+};
